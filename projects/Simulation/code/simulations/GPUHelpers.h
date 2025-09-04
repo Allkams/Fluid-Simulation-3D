55e5fc671c19670083ef9d043801867a7d5f2c9f
Allkams <Allkams02@gmail.com>
2025-09-04 20:56:14 +0200

    GPU Attempt2 - start

---
+++ projects/Simulation/code/simulations/GPUHelpers.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <GLFW/glfw3.h>
+#include <string>
+#include <vector>
+#include <iostream>
+
+namespace GPUHelpers 
+{
+	inline GLuint loadCommputeShader(const char* path)
+	{
+		/*std::ifstream file;
+
+		if(!file.is_open()) 
+		{
+
+		}*/
+	}
+}
